db419a44598997413530bc1350c3a08d01e4acc9
coderaryanjain <jainaryan312@gmail.com>
2025-07-22 11:38:42 +0530

    Initial Commit

---
+++ Weather_App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather_App
+//
+//  Created by Aryan Jain on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
